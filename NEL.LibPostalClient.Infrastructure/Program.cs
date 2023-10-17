338fdbb84e30af22191c4716db2686bbfe724397
Christo du Toit <christo@dutoit.co.uk>
2023-10-17 17:19:22 +0100

    INFRASTRUCTURE: Project Initialisation and Setup

---
+++ NEL.LibPostalClient.Infrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using NEL.LibPostalClient.Infrastructure.Services;
+
+namespace NEL.LibPostalClient.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
