c365a0852c02aed4bdc083384e8361def48cebc9
Christo du Toit <christo@dutoit.co.uk>
2023-10-18 13:08:03 +0100

    EXPOSERS: LibPostal Client

---
+++ NEL.LibPostalClient/Models/Foundations/LibPostal/Exceptions/LibPostalValidationException.cs
@@ -0,0 +1,15 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using Xeptions;
+
+namespace NEL.MESH.Models.Foundations.LibPostal.Exceptions
+{
+    internal class LibPostalValidationException : Xeption
+    {
+        public LibPostalValidationException(string message, Xeption innerException)
+            : base(message, innerException)
+        { }
+    }
+}
