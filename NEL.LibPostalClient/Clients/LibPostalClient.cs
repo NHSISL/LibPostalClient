badc8fe372a92214210b668daf2f7d355e7e43f8
Christo du Toit <christo@dutoit.co.uk>
2023-10-18 12:07:42 +0100

    CODE RUB: Folder rename and addition of Internal Visible

---
+++ NEL.LibPostalClient/Clients/LibPostalClient.cs
@@ -0,0 +1,10 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+namespace NEL.LibPostalClient.Clients
+{
+    public class LibPostalClient
+    {
+    }
+}
