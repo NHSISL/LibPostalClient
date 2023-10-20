2f94510f1c50a345a6982d5a60c1567bfce34824
Hassan Raza <h_raza110@outlook.com>
2023-10-20 15:31:41 +0100

    ShouldExpandAddressAsync -> PASS

---
+++ NEL.LibPostalClient.Tests.Acceptance/LibPostalTests.Logic.ExpandAddress.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using NEL.LibPostalClient.Models.Brokers.LibPostal;
+using Xunit;
+
+namespace NEL.LibPostalClient.Tests.Acceptance
+{
+    public partial class LibPostalTest
+    {
+        [Fact]
+        public async Task ShouldExpandAddressAsync()
+        {
+            //Given
+            string inputAddress = "10 Downing Str, Westminster, London, SW1A2AA, UK";
+            string[] expectedResult = { "10 downing str westminster london sw1a2aa uk", 
+                "10 downing str westminster london sw 1a2aa uk" };
+
+            //When
+            string[] actualResult = await this.libPostalClient.ExpandAddressAsync(inputAddress);
+
+            //Then
+            actualResult.Should().BeEquivalentTo(expectedResult);
+        }
+
+    }
+}
---
+++ NEL.LibPostalClient.Tests.Acceptance/LibPostalTests.cs
@@ -0,0 +1,31 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using NEL.LibPostalClient.Clients;
+using NEL.LibPostalClient.Models.Brokers.LibPostal;
+
+namespace NEL.LibPostalClient.Tests.Acceptance
+{
+    public partial class LibPostalTest
+    {
+        private readonly ILibPostalClient libPostalClient;
+
+        public LibPostalTest()
+        {
+            var config = new LibPostalConfiguration
+            {
+                DataDirectory = @"c:\temp\LipPostal",
+                ParserDataDirectory = @"c:\temp\LipPostal\parser",
+                LanguageClassifierDataDirectory = @"c:\temp\LipPostal\language_classifier",
+                PaserOptions = new ParserOptions
+                {
+                    Country = "GB",
+                    Language = "en",
+                }
+            };
+
+            libPostalClient = new Clients.LibPostalClient(config);
+        }
+    }
+}
