f10034a741cac2cac922b44447b80d2b69a335ed
Hassan Raza <h_raza110@outlook.com>
2023-10-23 09:21:01 +0100

    ShouldParseAddressAsync -> FAIL

---
+++ NEL.LibPostalClient.Tests.Acceptance/LibPostalTests.Logic.ParseAddress.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using CppSharp.Types.Std;
+using FluentAssertions;
+using NEL.LibPostalClient.Models.Brokers.LibPostal;
+using Xunit;
+
+namespace NEL.LibPostalClient.Tests.Acceptance
+{
+    public partial class LibPostalTest
+    {
+        [Fact]
+        public async Task ShouldParseAddressAsync()
+        {
+            //Given
+            string inputAddress = "10 downing str westminster london sw1a2aa uk";
+            List<KeyValuePair<string, string>> expectedResult = new List<KeyValuePair<string, string>>();
+
+            //expectedResult
+
+
+            //string[] expectedResult = { "10 downing str westminster london sw1a2aa uk", 
+            //    "10 downing str westminster london sw 1a2aa uk" };
+
+            //When
+            List<KeyValuePair<string, string>> actualResult = 
+                await this.libPostalClient.ParseAddressAsync(inputAddress);
+
+            //Then
+            actualResult.Should().BeEquivalentTo(expectedResult);
+        }
+
+    }
+}
